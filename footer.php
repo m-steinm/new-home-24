12ea06d54ac2bfb38f686f6fbc2a98752b9c11fa
Max <steinmetz.max@gmail.com>
2024-08-22 14:12:35 -0400

    Partial header and footer

---
+++ footer.php
@@ -0,0 +1,8 @@
+	</main>
+
+</body>
+<footer>
+	<h1>footr..</h1>
+</footer>
+
+</html>
---
+++ header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	<title>Document</title>
+
+	<link rel="stylesheet" href="site.css">
+</head>
+
+<body>
+	<header class="site-header">
+		<nav class="site-menu">
+			<ul>
+				<li>
+					<a href="index.php">home</a>
+				</li>
+				<li>
+					<a href="about.php">about</a>
+				</li>
+				<li>
+					<a href="contact.php">contact</a>
+				</li>
+			</ul>
+		</nav>
+	</header>
+
+	<main class="page-content">
