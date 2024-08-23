e155672b5c1d654d2a45c09cc24ca271b96cc606
Max <steinmetz.max@gmail.com>
2024-08-23 11:45:15 -0400

    Add example module

---
+++ graphic-diptych.php
@@ -0,0 +1,17 @@
+
+<graphic-diptych>
+
+<div class="graphic">
+	
+</div>
+
+<div class="content">
+	
+	<h2><?=$heading?></h2>
+
+	<p>
+		Lorem ipsum dolor sit, amet, consectetur adipisicing elit. Odit officiis aliquam quibusdam fugiat id veniam, officia veritatis ipsam soluta, molestiae atque nulla hic eligendi, eveniet ab voluptatum omnis fuga, aperiam.
+	</p>
+</div>
+
+</graphic-diptych>
