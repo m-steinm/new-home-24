264fe90d75aa2d87be94ae87581531e4b16bdfe9
Max <steinmetz.max@gmail.com>
2024-09-04 15:19:28 -0400

    Add dynamic data in project card

--- templates/components/project-card/project-card.php
+++ templates/components/project-card/project-card.php
@@ -1,9 +1,13 @@
+<?php
+	$title = $project['title'];
+?>
+
 
 <project-card>
 	<figure>
 		<img src="images/landscape.jpg" alt="">
 	</figure>
-	<h3 class=" ">Project Title</h3>
+	<h3 class=" "><?=$title?></h3>
 	<p class="calm-voice">project description</p>
 	<p class="calm-voice">Visit</p>
 </project-card>
--- templates/modules/project-spotlight/project-spotlight.php
+++ templates/modules/project-spotlight/project-spotlight.php
@@ -1,11 +1,36 @@
-
+<?php
+	$projects = [
+		[
+			"title" => "Project Title one",
+			"teaser" => "Example teaser",
+			"linkText" => "Visit me",
+			"url" => "#",
+		],
+		[
+			"title" => "Project Title two",
+			"teaser" => "Example teaser",
+			"linkText" => "Visit me",
+			"url" => "#",
+		],
+		[
+			"title" => "Project Title three",
+			"teaser" => "Example teaser",
+			"linkText" => "Visit me",
+			"url" => "#",
+		],		
+];
+?>
 <project-spotlight>
 
 	<h2 class="attention-voice">My Projects</h2>
 
 	<ul class="project-list">
+		<?php foreach ($projects as $project) { ?>
 		<li class="project">
-
+			<?php
+			include('templates/components/project-card/project-card.php');
+			?>
 		</li>
+		<?php } ?>
 	</ul>
 </project-spotlight>
