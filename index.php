36023e4bc522b89a98fda7b13ca004a48e8d58ad
Max <steinmetz.max@gmail.com>
2024-09-10 16:00:38 -0400

    Add blog module

--- index.php
+++ index.php
@@ -6,7 +6,10 @@
 		$pageDescription = "Web Design";
 		include('templates/modules/page-header/page-header.php');	?>
 
-<section>
+<section class="current-posts">
+	<inner-column>
+		<?php include('templates/modules/blog/blog.php'); ?>
+	</inner-column>
 
 </section>
 
