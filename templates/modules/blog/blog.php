344e4262c5964ad66388d881406e4d1bfd76ada8
Max <steinmetz.max@gmail.com>
2024-09-11 12:49:53 -0400

    Modify blog module

--- templates/modules/blog/blog.php
+++ templates/modules/blog/blog.php
@@ -10,21 +10,21 @@
 			"url" => "#",
 		],
 		[
-			"title" => "Blog title number one",
+			"title" => "Blog title number two",
 			"teaser" => "This is an example of a short sentance that might introduce a blog post",
 			"image" => "images/landscape.jpg",
 			"body" => "This is where the rest of the blog post would go. It could be really short. Or it could be really long. There isn't really a good way to represenet that here right now",
 			"url" => "#",
 		],
 		[
-			"title" => "Blog title number one",
+			"title" => "Blog title number three",
 			"teaser" => "This is an example of a short sentance that might introduce a blog post",
 			"image" => "images/landscape.jpg",
 			"body" => "This is where the rest of the blog post would go. It could be really short. Or it could be really long. There isn't really a good way to represenet that here right now",
 			"url" => "#",
 		],
 				[
-			"title" => "Blog title number one",
+			"title" => "Blog title number four",
 			"teaser" => "This is an example of a short sentance that might introduce a blog post",
 			"image" => "images/landscape.jpg",
 			"body" => "This is where the rest of the blog post would go. It could be really short. Or it could be really long. There isn't really a good way to represenet that here right now",
@@ -35,18 +35,19 @@
 
  
 <!-- //custom element -->
-<blog-spotlight>
-	<!-- //heading -->
-	<h2>Writing</h2>
-	<!-- //unordered list -->
-		<ul>
-			<!-- //function for displaying blog posts pulled from data -->
-			<?php foreach ($posts as $post){ ?>
-				<li>
-				<?php	include('templates/components/blog-card/blog-card.php');
-			?>
-				</li>
-			<?php } ?>
-			<!-- //put each blog post in a list item -->
-		</ul>
-</blog-spotlight>
+	<blog-spotlight>
+		<!-- //heading -->
+		<h2>Writing</h2>
+		<!-- //unordered list -->
+			<ul class = 'blog-grid'>
+				<!-- //function for displaying blog posts pulled from data -->
+				<?php foreach ($posts as $post){ ?>
+					<li>
+					<?php	include('templates/components/blog-card/blog-card.php');
+				?>
+					</li>
+				<?php } ?>
+				<!-- //put each blog post in a list item -->
+			</ul>
+	</blog-spotlight>
+
